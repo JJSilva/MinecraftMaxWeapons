16acd8ac49f403f8ccf27f53d356432d8ec1353e
Justin Silva <JJSilva@Gmail.com>
2023-09-14 09:28:00 -0700

    bug fixes

--- MinecraftMaxWeapons/Program.cs
+++ MinecraftMaxWeapons/Program.cs
@@ -19,14 +19,12 @@ namespace MinecraftMaxWeapons
 
             Console.WriteLine("Giving Items");
 
-            string sword = "netherite_sword{Enchantments:[{id:sharpness,lvl:9999},{id:smite,lvl:9999},{id:bane_of_arthropods,lvl:9999},{id:cleaving,lvl:9999},{id:sweeping,lvl:9999},{id:fire_aspect,lvl:9999},{id:looting,lvl:9999},{id:thorns,lvl:9999}],Unbreakable:1} 1";
+            
+            string sword = "netherite_sword{ Enchantments: [{ id: sharpness,lvl: 9999},{ id: smite,lvl: 9999},{ id: bane_of_arthropods,lvl: 9999},{ id: cleaving,lvl: 9999},{ id: sweeping,lvl: 9999},{ id: fire_aspect,lvl: 9999},{ id: looting,lvl: 9999},{ id: thorns,lvl: 9999}],Unbreakable: 1} 1";
             output.AppendLine(String.Format("give {0} {1}", username, sword));
 
-            string shovel = "minecraft:netherite_shovel{Unbreakable:1,Enchantments:[{id:efficiency,lvl:1000},{id:fortune,lvl:1000}]};";
+            string shovel = "minecraft:netherite_shovel{Unbreakable:1,Enchantments:[{id:efficiency,lvl:1000},{id:fortune,lvl:1000}]} 1";
             output.AppendLine(String.Format("give {0} {1}", username, shovel));
-
-            string elytra = "minecraft:elytra{ Unbreakable: 1,Enchantments: [{ id: mending,lvl: 1000}]}";
-            output.AppendLine(String.Format("give {0} {1}", username, elytra));
 
             string axe = "netherite_axe{Enchantments:[{id:sharpness,lvl:9999},{id:smite,lvl:9999},{id:bane_of_arthropods,lvl:9999},{id:thorns,lvl:9999},{id:fire_aspect,lvl:9999},{id:looting,lvl:9999},{id:fortune,lvl:9999},{id:efficiency,lvl:9999}],Unbreakable:1} 1";
             output.AppendLine(String.Format("give {0} {1}", username, axe));
@@ -34,7 +32,7 @@ namespace MinecraftMaxWeapons
             string shield = "shield{BlockEntityTag:{Base:3,Patterns:[{Pattern:\"dls\",Color:0},{Pattern:\"gru\",Color:3},{Pattern:\"gra\",Color:0},{Pattern:\"bo\",Color:15}]},Enchantments:[{id:sharpness,lvl:9999},{id:thorns,lvl:9999},{id:looting,lvl:9999}],Unbreakable:1} 1";
             output.AppendLine(String.Format("give {0} {1}", username, shield));
 
-            string pickaxe = "give @p diamond_pickaxe{Enchantments:[{id:efficiency,lvl:1000},{id:unbreaking,lvl:1000},{id:fortune,lvl:1000}]}";
+            string pickaxe = "netherite_pickaxe{Enchantments:[{id:efficiency,lvl:1000},{id:unbreaking,lvl:1000},{id:fortune,lvl:1000}]} 1";
             output.AppendLine(String.Format("give {0} {1}", username, pickaxe));
 
             string bow = "bow{Enchantments:[{id:power,lvl:99999},{id:sharpness,lvl:9999},{id:thorns,lvl:9999},{id:infinity,lvl:9999}],Unbreakable:1} 1";
@@ -52,6 +50,9 @@ namespace MinecraftMaxWeapons
             string boots = "netherite_boots{Enchantments:[{id:protection,lvl:9999},{id:feather_falling,lvl:9999},{id:respiration,lvl:9999},{id:depth_strider,lvl:9999},{id:aqua_affinity,lvl:9999},{id:thorns,lvl:9999}],Unbreakable:1} 1";
             output.AppendLine(String.Format("give {0} {1}", username, boots));
 
+            string elytra = "minecraft:elytra{Unbreakable: 1,Enchantments: [{ id: mending,lvl: 1000}]} 1";
+            output.AppendLine(String.Format("give {0} {1}", username, elytra));
+
             string crossbow = "crossbow{Enchantments:[{id:quick_charge,lvl:5},{id:multishot,lvl:10},{id:piercing,lvl:10},{id:knockback,lvl:5},{id:fire_aspect,lvl:9999},{id:thorns,lvl:9999}],Unbreakable:1} 1";
             output.AppendLine(String.Format("give {0} {1}", username, crossbow));
 
@@ -61,8 +62,11 @@ namespace MinecraftMaxWeapons
             string fireworks = "minecraft:firework_rocket 256";
             output.AppendLine(String.Format("give {0} {1}", username, fireworks));
 
-            string effects = "experience add @s 999 levels\neffect give @p absorption 1000000 4 true\neffect give @p conduit_power 1000000 255 true\neffect give @p regeneration 1000000 255 true\neffect give @p resistance 1000000 255 true\neffect give @p speed 1000000 1 true\neffect give @p strength 1000000 255 true\neffect give @p water_breathing 1000000 255 true\neffect give @p dolphins_grace 1000000 1 true\neffect give @p fire_resistance 1000000 255 true\neffect give @p glowing 1000000 255 true\neffect give @p haste 1000000 255 true\neffect give @p hero_of_the_village 99999 255 true\neffect give @p instant_damage 99999 255 true\neffect give @p instant_health 99999 255 true\neffect give @p jump_boost 99999 1 true\neffect give @p luck 99999 255 true\neffect give @p night_vision 99999 255 true";
-            output.AppendLine(String.Format("give {0} {1}", username, effects));
+            string effects = "absorption 1000000 4 true\neffect give @p conduit_power 1000000 255 true\neffect give @p regeneration 1000000 255 true\neffect give @p resistance 1000000 255 true\neffect give @p speed 1000000 1 true\neffect give @p strength 1000000 255 true\neffect give @p water_breathing 1000000 255 true\neffect give @p dolphins_grace 1000000 1 true\neffect give @p fire_resistance 1000000 255 true\neffect give @p glowing 1000000 255 true\neffect give @p haste 1000000 255 true\neffect give @p hero_of_the_village 99999 255 true\neffect give @p instant_damage 99999 255 true\neffect give @p instant_health 99999 255 true\neffect give @p jump_boost 99999 1 true\neffect give @p luck 99999 255 true\neffect give @p night_vision 99999 255 true";
+            output.AppendLine(String.Format("effect give {0} {1}", username, effects));
+
+            string experince = "999 levels";
+            output.AppendLine(String.Format("experience add {0} {1}", username, experince));
 
 
 
@@ -76,6 +80,8 @@ namespace MinecraftMaxWeapons
                 sw.WriteLine(output.ToString());
             }
 
+            Console.WriteLine(output.ToString());
+ 
             Console.ReadLine();
 
         }
